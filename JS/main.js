7929486d4a3c5610c15ce7086a0f69494e406855
Peter Laptop <Peter.P.Maltby@gmail.com>
2017-10-18 14:38:30 +0100

    got page to load without flashing

--- JS/main.js
+++ JS/main.js
@@ -5,6 +5,7 @@
 $(document).ready(function(){
     var de = 1000;
     $("#title,footer,nav li").hide();
+    $("html").css("display","block");
     $("#title").fadeIn(de);
     $("footer").slideDown(de).css('display','none').fadeIn(de*1.5).dequeue();
     $('nav li').show().fadeTo(0,0).delay(900).each(function( i ) {
