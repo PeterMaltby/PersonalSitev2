57ef6f02242a7b0103a56bf8b8419a26a184c147
Peter Laptop <Peter.P.Maltby@gmail.com>
2017-10-18 19:03:26 +0100

    finished for now

--- JS/main.js
+++ JS/main.js
@@ -4,14 +4,14 @@
 
 $(document).ready(function(){
     var de = 1000;
-    $("#title,footer,nav li").hide();
     $("html").css("display","block");
+    $("#title,footer,nav li,.text").hide();
     $("#title").fadeIn(de);
     $("footer").slideDown(de*1.5).css('display','none').fadeIn(de*1.5).dequeue();
     $('nav li').show().fadeTo(0,0).delay(900).each(function( i ) {
         $(this).delay( i * 100 ).fadeTo(de,1.0);
     });
-
+    $(".text").fadeIn(de);
 
 
     $('a').click(function (e) {
