8d58174b24f29075dc8bf182570b536c960ccef6
Peter Laptop <Peter.P.Maltby@gmail.com>
2017-10-18 19:40:17 +0100

    JS completed for main pages; still bug with flashing but greatly reduced. links repaired; title tag fixed.

---
+++ JS/main.js
@@ -0,0 +1,29 @@
+$(document).ready(function(){
+    var de = 1000;
+    $("html").css("display","block");
+    $("#title,footer,nav li,.jsFlow").hide();
+    $("#title").fadeIn(de);
+    $("footer").slideDown(de*1.5).css('display','none').fadeIn(de*1.5).dequeue();
+    $('nav li').show().fadeTo(0,0).delay(900).each(function( i ) {
+        $(this).delay( i * 100 ).fadeTo(de,1.0);
+    });
+
+    $('.jsFlow').show().fadeTo(0,0).delay(900).each(function( x ) {
+        $(this).delay( x * 250 ).fadeTo(de,1.0);
+    });
+
+    //$(".text").fadeIn(de);
+
+
+    $('a').click(function (e) {
+        e.preventDefault();
+        var goTo = this.getAttribute("href");
+        
+        $("*:not(title)").fadeOut(de);
+   
+        setTimeout(function(){
+            window.location = goTo;
+        },de);       
+    }); 
+
+});
