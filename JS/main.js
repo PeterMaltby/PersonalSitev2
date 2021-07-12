46284949bd81d6b6af05a581a9e1f25f5b4e918d
Peter Maltby <Peter.p.Maltby@gmail.com>
2021-07-12 12:44:02 +0100

    javascript update removed fade code, added email save to clipboard function.

--- JS/main.js
+++ JS/main.js
@@ -3,39 +3,22 @@ function isTouchDevice() {
 }
 
 $(document).ready(function(){
-    var de = 1000;
-
-    $(".touch").show();
-    if (isTouchDevice){
-        $(".touch").hide();
-    }
-
-    $("html").css("display","block");
-    //$("#title,footer,nav li,.jsFlow").hide();
-    $("#title").hide().fadeIn(de);
-    $("footer").hide().fadeIn(de);
-    $('nav li').hide().delay(900).each(function( i ) {
-        $(this).delay( i * 100 ).fadeIn(de);
-    });
-
-    $('.jsFlow').hide().delay(900).each(function( x ) {
-        $(this).delay( x * 250 ).fadeTo(de,1.0);
-    });
-
-    //$(".text").fadeIn(de);
-
-
-    $('a').click(function (e) {
-        e.preventDefault();
-        var goTo = this.getAttribute("href");
-        
-        $("*:not(title)").fadeOut(de);
-   
-        setTimeout(function(){
-            window.location = goTo;
-        },de);       
-    }); 
 
 });
 
+function copyEmail() {
+    var copyText = document.createElement("textarea");
+    copyText.value = "MY EMAIL!";
+    copyText.setAttribute("readonly",'');
+    copyText.style.position = "absolute";
+    copyText.style.position = "-9999px";
+    document.body.appendChild(copyText);
+    copyText.select();
+    copyText.setSelectionRange(0,99999);
 
+    document.execCommand("copy");
+
+    alert("email saved to clipboard.");
+
+    document.body.removeChild(copyText);
+}
